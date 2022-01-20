39c4270738fba91d27a07bbfb4971b1dcdb5cea0
Abhay <abahyreso10@gmail.com>
2022-01-21 01:16:39 +0530

    Co-authored-by: abhisheksaroniya <abhisheksaroniya@users.noreply.github.com> Co-authored-by: Akanksha priya <Spring-dot@users.noreply.github.com>

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,11 @@
 import React from 'react';
-
+import "./Login.css";
+import Button from 'react-bootstrap/Button';
 function Login() {
-  return <div>
-      
+  return <div className='Login'>
+      <a className='btn btn-primary loginbutton' href=''>Login</a>
+      <a className='btn btn-primary signupbutton' href=''>Already have an account/signup</a>
+
   </div>;
 }
 
